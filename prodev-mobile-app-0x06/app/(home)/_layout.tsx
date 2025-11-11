690dcdb6d5c6ba1da09ba9bb794fe20184ffdfe5
Elijah <ellayab804@gmail.com>
2025-11-12 00:37:48 +0300

    implement styling with nativewind for tailwind css modify layout.tsx file

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -5,6 +5,7 @@ const HomeRootLayout = () => {
   return (
     <Tabs screenOptions={{
         tabBarActiveTintColor: '#34967C',
+        headerShown: false
     }} >
       <Tabs.Screen name="index" options={{
         title: 'Home',
